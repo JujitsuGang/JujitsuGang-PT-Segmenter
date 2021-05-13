4f41cebbc6ce25ad1a4b0a8e49f975c1cd59589b
JujitsuGang <105345817+JujitsuGang@users.noreply.github.com>
2021-05-13 21:21:41 +0000

    Libraries

--- src/segmentador/finetune.py
+++ src/segmentador/finetune.py
@@ -161,4 +161,105 @@ def finetune(
     noise_start_token: str = "[NOISE_START]",
     noise_end_token: str = "[NOISE_END]",
 ):
-    """Fine-tune a pretraine
+    """Fine-tune a pretrained model in new data."""
+    if len(segments) == 0:
+        return model
+
+    if isinstance(segments[0], str):
+        segments = [segments]
+
+    (seg_input_ids, seg_labels) = text_to_ids(
+        segments=segments,
+        tokenizer=tokenizer,
+        noise_start_token=noise_start_token,
+        noise_end_token=noise_end_token,
+    )
+
+    (input_ids, labels) = ids_to_insts(
+        seg_input_ids=seg_input_ids,
+        seg_labels=seg_labels,
+        inst_length=inst_length,
+        pad_id=tokenizer.pad_token_id,
+    )
+
+    assert len(input_ids) == len(labels)
+
+    n = len(input_ids)
+
+    loss_fn = torch.nn.CrossEntropyLoss(ignore_index=-100, reduction="none")
+    optim = torch.optim.Adam(model.parameters(), lr=lr)
+
+    indices = torch.arange(n)
+    weights = torch.ones(n, dtype=torch.double)
+
+    dl_train = torch.utils.data.DataLoader(
+        torch.utils.data.TensorDataset(indices, weights),
+        shuffle=True,
+        batch_size=batch_size,
+        drop_last=False,
+    )
+
+    model.train()
+    model.to(device)
+
+    if early_stopping_accuracy_threshold is None:
+        early_stopping_accuracy_threshold = np.inf
+
+    for i in range(1, 1 + max_epochs):
+        mov_loss = None
+        optim.zero_grad()
+        pbar = tqdm.tqdm(dl_train, disable=not show_progress_bar)
+        total_misclass = 0
+        total_insts = 0
+
+        for j, (batch_inds, batch_weight) in enumerate(pbar, 1):
+            batch_inds = batch_inds.squeeze()
+            batch_inds = torch.atleast_1d(batch_inds)
+
+            cur_input_ids = input_ids[batch_inds, :]
+            batch_weight = batch_weight.to(device)
+
+            batch = {"input_ids": cur_input_ids}
+
+            if is_complete_input:
+                batch["token_type_ids"] = torch.zeros_like(cur_input_ids)
+                batch["attention_mask"] = torch.ones_like(cur_input_ids)
+
+            batch = {k: torch.atleast_2d(v).to(device) for k, v in batch.items()}
+
+            y_true = labels[batch_inds, :].to(device)
+            y_true = y_true.view(-1)
+
+            output = model(**batch)
+            logits = output["logits"].view(-1, 4)
+
+            assert logits.shape[0] == y_true.numel(), (logits.shape, y_true.shape)
+
+            loss = loss_fn(logits, y_true)
+            loss = loss.reshape(-1, inst_length).mean(axis=-1)
+            loss = (loss * batch_weight).sum() / (batch_weight.sum() * grad_acc_its)
+            loss.backward()
+
+            if j % grad_acc_its == 0 or j == n:
+                torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
+                optim.step()
+                optim.zero_grad()
+
+            with torch.no_grad():
+                y_preds = logits.detach().argmax(axis=-1)
+                assert y_preds.shape == y_true.shape
+                insts_with_misclass = ((y_preds != y_true) * (y_true != -100)).view(-1, inst_length)
+                insts_with_misclass = insts_with_misclass.any(axis=-1)
+                assert insts_with_misclass.ndim == 1 and int(insts_with_misclass.numel()) == len(batch_inds)
+                insts_with_misclass = torch.nonzero(insts_with_misclass)
+                insts_with_misclass = insts_with_misclass.cpu()
+                insts_with_misclass = batch_inds[insts_with_misclass]
+
+                if focus_on_misclassifications:
+                    weights[insts_with_misclass] += 1.0
+
+                total_misclass += int(insts_with_misclass.numel())
+                total_insts += int(batch_inds.numel())
+
+            accuracy = 1.0 - total_misclass / total_insts
+            pbar.set_description(
