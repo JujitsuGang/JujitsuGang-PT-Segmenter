654c485bdcbcade16e2bcf9b825ddc9e2e49aa7c
JujitsuGang <105345817+JujitsuGang@users.noreply.github.com>
2021-05-28 17:12:02 +0000

    Pontoon: Update Malay (ms) localization of Firefox Screenshots

--- src/segmentador/optimize/quantize.py
+++ src/segmentador/optimize/quantize.py
@@ -599,3 +599,241 @@ def quantize_lstm_model_as_torch(
         torch.nn.LSTM,
         torch.nn.Linear,
     ),
+    check_cached: bool = True,
+    verbose: bool = False,
+) -> QuantizationOutputTorch:
+    """Create a quantized LSTMSegmenter model as Torch format.
+
+    Models created from this format can be loaded for inference as:
+
+    >>> optimize.TorchJITLSTMSegmenter(  # doctest: +SKIP
+    ...     uri_model='<quantized_model_uri>',
+    ...     ...,
+    ... )
+
+    Parameters
+    ----------
+    model : segmenter.LSTMSegmenter
+        LSTMSegmenter model to be quantized.
+
+    quantized_model_filename : t.Optional[str], default=None
+        Output filename. If None, a long and descriptive name will be derived from model's
+        parameters.
+
+    quantized_model_dirpath : str, default='./quantized_models'
+        Path to output file directory, which the resulting quantized model will be stored,
+        alongside any possible coproducts also generated during the quantization procedure.
+
+    modules_to_quantize : t.Tuple[t.Type[torch.nn.Module], ...], \
+        default=(torch.nn.Embedding, torch.nn.LSTM, torch.nn.Linear)
+
+    check_cached : bool, default=True
+        If True, check whether a model with the same model exists before quantization.
+        If this happens to be the case, this function will not produce any new models.
+
+    verbose : bool, default=False
+        If True, print information regarding the results.
+
+    Returns
+    -------
+    paths : t.Tuple[str, ...]
+        File URIs related from generated files during the quantization procedure. The
+        final model URI can be accessed from the `output_uri` attribute.
+    """
+
+    model_attributes: t.Dict[str, t.Any] = collections.OrderedDict(
+        (
+            ("num_layers", model.lstm_hidden_layer_size),
+            ("vocab_size", model.tokenizer.vocab_size),
+            ("num_layers", model.lstm_num_layers),
+        )
+    )
+
+    paths = _build_torch_default_uris(
+        model_name="lstm",
+        model_attributes=model_attributes,
+        quantized_model_dirpath=quantized_model_dirpath,
+        quantized_model_filename=quantized_model_filename,
+    )
+
+    if check_cached and os.path.isfile(paths.output_uri):
+        if verbose:  # pragma: no cover
+            print(f"Found cached model in '{paths.output_uri}'. Skipping model quantization.")
+
+        return paths
+
+    pytorch_module = model.model
+
+    if torch.nn.Embedding in modules_to_quantize:
+        pytorch_module.embeddings.qconfig = (  # type: ignore
+            torch.quantization.float_qparams_weight_only_qconfig
+        )
+
+    quantized_pytorch_module = torch.quantization.quantize_dynamic(
+        pytorch_module,
+        set(modules_to_quantize),
+        dtype=torch.qint8,
+    )
+
+    dummy_input_ids, _, _ = _gen_dummy_inputs_for_tracing(
+        batch_size=3,
+        vocab_size=model.tokenizer.vocab_size,
+        seq_length=512,
+    )
+
+    dummy_input_ids = dummy_input_ids.unsqueeze(1)
+
+    jit_traced_model = torch.jit.trace(
+        func=torch.jit.script(quantized_pytorch_module),
+        example_inputs=(dummy_input_ids[0],),
+        check_inputs=[(dummy_input_ids[1],), (dummy_input_ids[2],)],
+        strict=True,
+    )
+    pickled_tokenizer = pickle.dumps(model.tokenizer, protocol=pickle.HIGHEST_PROTOCOL)
+
+    torch.jit.save(
+        m=jit_traced_model,
+        f=paths.output_uri,
+        _extra_files={"tokenizer": pickled_tokenizer},
+    )
+
+    if verbose:  # pragma: no cover
+        c_ylw = colorama.Fore.YELLOW if colorama else ""
+        c_blu = colorama.Fore.BLUE if colorama else ""
+        c_rst = colorama.Style.RESET_ALL if colorama else ""
+
+        module_name = ".".join(__name__.split(".")[:-1])
+
+        print(
+            "Saved quantized Pytorch module (Torch JIT format) in "
+            f"{c_blu}'{paths.output_uri}'{c_rst}. "
+            "To use it, load a LSTM segmenter model as:\n\n"
+            f"{module_name}.{models.TorchJITLSTMSegmenter.__name__}(\n"
+            f"   {c_ylw}uri_model={c_blu}'{paths.output_uri}'{c_rst},\n"
+            "   ...,\n"
+            ")"
+        )
+
+    return paths
+
+
+def quantize_model(
+    model: t.Union[segmenter.BERTSegmenter, segmenter.LSTMSegmenter],
+    quantized_model_filename: t.Optional[str] = None,
+    quantized_model_dirpath: str = "./quantized_models",
+    model_output_format: str = "onnx",
+    onnx_opset_version: int = 17,
+    check_cached: bool = True,
+    verbose: bool = False,
+    **kwargs: t.Any,
+) -> QuantizationOutput:
+    """Generate a quantized segmenter model from an existing segmenter model.
+
+    This function will derive the correct quantization function from the provided
+    model type (BERT or LSTM), and the `model_output_format` parameter value. Check
+    ``See Also`` section for a list of specific quantization functions.
+
+    Parameters
+    ----------
+    model : segmenter.BERTSegmenter or segmenter.LSTMSegmenter
+        Segmenter model to quantize.
+
+    quantized_model_filename : str or None, default=None
+        Output filename. If None, a long and descriptive name will be derived from model's
+        parameters.
+
+    quantized_model_dirpath : str, default='./quantized_models'
+        Path to output file directory, which the resulting quantized model will be stored,
+        alongside any possible coproducts also generated during the quantization procedure.
+
+    model_output_format : {'onnx', 'torch_jit'}, default='onnx'
+        Output format of quantized model. This option also determines how exactly inference with
+        the quantized model will be done. See ``See Also`` section for information about specific
+        configuratins of model types and output formats.
+
+    onnx_opset_version: int, default=17
+        ONNX operator set version. Used only if `model_output_format='onnx'`. Check [2]_ for
+        more information.
+
+    check_cached : bool, default=True
+        If True, check whether a model with the same model exists before quantization.
+        If this happens to be the case, this function will not produce any new models.
+
+    verbose : bool, default=False
+        If True, print information regarding the results.
+
+    **kwargs : dict
+        Additional parameters passed to quantization function.
+
+    Returns
+    -------
+    paths : t.Tuple[str, ...]
+        Named tuple with all paths of files generated during the full quantization
+        procedure.
+
+    See Also
+    --------
+    quantize_lstm_model_as_onnx : quantize LSTMSegmenter as model_output_format='onnx'.
+    quantize_lstm_model_as_torch : quantize LSTMSegmenter as model_output_format='torch_jit'.
+    quantize_bert_model_as_onnx : quantize BERTSegmenter as model_output_format='onnx'.
+    quantize_bert_model_as_torch : quantize BERTSegmenter as model_output_format='torch_jit'.
+
+    References
+    ----------
+    .. [1] Graph Optimizations in ONNX Runtime. Available at:
+       https://onnxruntime.ai/docs/performance/graph-optimizations.html
+
+    .. [2] ONNX Operator Schemas. Available at:
+       https://github.com/onnx/onnx/blob/main/docs/Operators.md
+    """
+    if not isinstance(model, (segmenter.BERTSegmenter, segmenter.LSTMSegmenter)):
+        raise TypeError(
+            f"Unknown segmenter type for quantization: '{type(model)}'. Please "
+            "provide either BERTSegmenter or LSTMSegmenter."
+        )
+
+    if model_output_format not in {"onnx", "torch_jit"}:
+        raise ValueError(
+            f"Unsupported 'model_output_format={model_output_format}'. "
+            "Please choose either 'onnx' or 'torch_jit'."
+        )
+
+    fn_kwargs: t.Dict[str, t.Any] = {
+        "model": model,
+        "quantized_model_filename": quantized_model_filename,
+        "quantized_model_dirpath": quantized_model_dirpath,
+        "check_cached": check_cached,
+        "verbose": verbose,
+    }
+
+    fn_quantization_factory: t.Dict[
+        t.Tuple[t.Type[_base.BaseSegmenter], str], t.Callable[..., QuantizationOutput]
+    ] = {
+        (segmenter.BERTSegmenter, "torch_jit"): quantize_bert_model_as_torch,
+        (segmenter.BERTSegmenter, "onnx"): quantize_bert_model_as_onnx,
+        (segmenter.LSTMSegmenter, "torch_jit"): quantize_lstm_model_as_torch,
+        (segmenter.LSTMSegmenter, "onnx"): quantize_lstm_model_as_onnx,
+    }
+
+    try:
+        fn_quantization = fn_quantization_factory[(type(model), model_output_format)]
+
+    except KeyError as e_key:
+        raise ValueError(
+            f"Unsupported 'model_output_format={model_output_format}' for segmenter "
+            f"type={type(model)}."
+        ) from e_key
+
+    if model_output_format == "onnx" and isinstance(model, segmenter.LSTMSegmenter):
+        v_major, v_minor, _ = platform.python_version_tuple()
+        fn_kwargs["onnx_opset_version"] = onnx_opset_version
+
+        if 100 * int(v_major) + int(v_minor) < 310 and onnx_opset_version > 15:
+            warnings.warn(
+                f"Unsupported onnx_opset_version={onnx_opset_version} for Python version < 3.10 "
+                f"(detected '{v_major}.{v_minor}'). Setting it to '15'.",
+                UserWarning,
+            )
+            fn_kwargs["onnx_opset_version"] = 15
+
+    return fn_quantization(**fn_kwargs, **kwargs)
---
+++ src/segmentador/output_handlers/__init__.py
@@ -0,0 +1,5 @@
+# pylint: disable='missing-module-docstring'
+from .poolers import *
+from .packer import *
+from .noise import *
+from . import postprocessors
